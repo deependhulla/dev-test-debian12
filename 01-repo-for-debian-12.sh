c34a6b3d803548896463de948b1fa0f73b27e677
Deepen Dhulla <deepen@deependhulla.com>
2023-06-21 18:03:32 +0530

    update on Wed Jun 21 18:03:32 IST 2023

--- 01-repo-for-debian-12.sh
+++ 01-repo-for-debian-12.sh
@@ -18,7 +18,7 @@ echo "deb-src http://deb.debian.org/debian/ bookworm-updates main non-free-firmw
 apt update
 apt -y upgrade
 ## few tools need for basic 
-apt -y install vim  screen net-tools git mc
+apt -y install vim  openssh-server screen net-tools git mc
 ## install insstead of systemd-timesyncd for better time sync
 apt -y  install chrony  2>/dev/null 1>/dev/null
 ## -x option added to allow in LXC
